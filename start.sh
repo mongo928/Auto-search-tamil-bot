2ad28994ea03e7ff07579dda1ce442106637c271
mongo928 <132264693+mongo928@users.noreply.github.com>
2023-09-19 13:44:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mongo928/Auto-search-tamil-bot
+  git clone https://github.com/mongo928/Auto-search-tamil-bot.git /Auto-search-tamil-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-search-tamil-bot
